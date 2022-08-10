b32edd733887e779b84ab7dddecf750ade5d9e2d
HaHyunkyung <gyh3824@naver.com>
2022-08-10 17:22:59 +0900

    정렬 Integer[], 내림차순 백준 25305

---
+++ day0712/src/main/java/day0810/day0810.java
@@ -0,0 +1,27 @@
+package day0810;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class day0810 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int cut = sc.nextInt();
+		
+		Integer[] arr = new Integer [N];
+	
+		for(int i=0;i<N;i++) {
+			
+				arr[i] = sc.nextInt();	
+				
+		}
+		
+		Arrays.sort(arr,Collections.reverseOrder());
+		
+		System.out.println(arr[cut-1]);
+	}
+}
